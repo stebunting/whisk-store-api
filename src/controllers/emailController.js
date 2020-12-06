1873208a18091546cf80fc934a1dfee051d278db
Stephen Bunting <stebunting@gmail.com>
2020-12-06 21:35:16 +0100

    Added error logging to emailController

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -4,6 +4,7 @@ const tag = 'store-api:emailController';
 // Requirements
 const nodemailer = require('nodemailer');
 const ejs = require('ejs');
+const log = require('winston');
 const debug = require('debug')(tag);
 const { priceFormat, parseDateCode, capitaliseFirst } = require('../functions/helpers');
 
@@ -17,11 +18,15 @@ let transporter;
 function isConnected() {
   return new Promise((resolve, reject) => {
     if (transporter == null) {
+      log.error('Attempting to use emailer while disconnected', { metadata: { tag } });
       return reject(new Error('Not connected to SMTP server'));
     }
     return transporter.verify()
       .then((data) => resolve(data))
-      .catch((error) => reject(error));
+      .catch((error) => {
+        log.error('Error verifying email transporter', { metadata: { tag, error } });
+        return reject(error);
+      });
   });
 }
 
@@ -38,24 +43,25 @@ function connect() {
   return new Promise((resolve, reject) => {
     isConnected()
       .then((data) => resolve(data))
-      .catch((error) => reject(error));
+      .catch((error) => {
+        log.error('Error verifying new email transporter', { metadata: { tag, error } });
+        return reject(error);
+      });
   });
 }
 
 function sendEmail(config) {
   return new Promise((resolve, reject) => {
-    isConnected()
-      .then((data) => {
-        if (data) {
-          const emailConfig = {
-            ...config,
-            bcc: process.env.EMAIL_BCC
-          };
-          return transporter.sendMail(emailConfig);
-        }
-        return reject(data);
-      })
-      .then((response) => resolve(response))
+    isConnected().then((data) => {
+      if (data) {
+        const emailConfig = {
+          ...config,
+          bcc: process.env.EMAIL_BCC
+        };
+        return transporter.sendMail(emailConfig);
+      }
+      return reject(data);
+    }).then((response) => resolve(response))
       .catch((error) => reject(error));
   });
 }
@@ -78,7 +84,7 @@ async function sendConfirmationEmail(order) {
       priceFormat
     });
   } catch (error) {
-    debug(error);
+    log.error('Error templating email', { metadata: { tag, error, order } });
   }
 
   const emailConfig = {
@@ -89,10 +95,15 @@ async function sendConfirmationEmail(order) {
     html
   };
 
-  const response = await sendEmail(emailConfig);
-  const correctResponse = response.response.substr(0, 6) === '250 OK';
-  const containsEmailAddress = response.accepted.includes(order.details.email);
-  return correctResponse && containsEmailAddress;
+  try {
+    const response = await sendEmail(emailConfig);
+    const correctResponse = response.response.substr(0, 6) === '250 OK';
+    const containsEmailAddress = response.accepted.includes(order.details.email);
+    return correctResponse && containsEmailAddress;
+  } catch (error) {
+    log.error('Error sending email', { metadata: { tag, error, emailConfig } });
+    throw error;
+  }
 }
 
 module.exports = { isConnected, connect, sendConfirmationEmail };
